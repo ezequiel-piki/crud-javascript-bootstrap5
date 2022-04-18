d6afec96c8835c69239b9ca35a18d4941bf9553f
Ezequiel <ezequielpiki23@gmail.com>
2022-04-17 21:48:38 -0300

    agrego nuevos archivos al proyecoy, index.html,app.js,firebaseConfig.js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+const nombreUsuario = document.getElementById ('nombreUsuario');
+const btnIngreso = document.getElementById('btnIngreso');
+const btnCerrarSesion = document.getElementById ('btnCerrarSesion')
+const contenidoWeb = document.getElementById ('contenidoWeb');
+const formulario = document.getElementById('formulario');
+const texto = document.getElementById('texto'); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
